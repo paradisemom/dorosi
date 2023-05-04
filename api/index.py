c3250c68c5232c6defca25ce50cfe66204fef961
paradisemom <111268812+paradisemom@users.noreply.github.com>
2023-05-04 15:14:00 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -30,7 +30,7 @@ def callback():
 
     # handle webhook body
     try:
-        handler.handle(body, signature)
+        webhook_handler.handle(body, signature)
     except InvalidSignatureError:
         print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
