c11c6f8a4be552ce3a1ea1adeddc2c0c5ff4e67e
paradisemom <111268812+paradisemom@users.noreply.github.com>
2023-05-04 15:10:00 +0800

    a

--- api/index.py
+++ api/index.py
@@ -38,7 +38,7 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
+@webhook_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
